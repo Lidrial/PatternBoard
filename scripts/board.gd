7b2913f2d3015114dbd2e2f9b4dc9a94e8d72075
royjah <eltonroy.h@gmail.com>
2024-11-26 19:45:13 +0100

    Board_update
    -ajout d'un asset pour une case en 32x32 et sa version grisé (a voir pour la taille)
    -ajout et test script cell (pour individualiser chaque cellule, se référer a la position de celle ci)
    -modfication Board script, et ajout du noeud GridContainer en tant qu'enfant

--- scripts/board.gd
+++ scripts/board.gd
@@ -1,22 +1,16 @@
-#pour test final, supprimer le noeud grid container et ses enfants dans la scéne board
-#problème a venir, on ne peut pas changer la propriété des ancrages, via le script
-
 extends Control
 
-const CASE_SCENE := preload("res://scenes/case.tscn")
+const CELL_SCENE := preload("res://scenes/cell.tscn")
 const BOARD_SIZE := 16
 
 func _ready():
-	var grid = GridContainer.new()
+	var grid = $GridContainer
 	grid.columns = BOARD_SIZE
-	add_child(grid)
 	
 	for i in range(BOARD_SIZE * BOARD_SIZE):
-		var case_instance = CASE_SCENE.instantiate()
-		#case_instance.id = i #permet de nommer la case en fonction de N répétition, NE MARCHE PAS
-		grid.add_child(case_instance)
+		var cell_instance = CELL_SCENE.instantiate()
+		grid.add_child(cell_instance)
 	pass 
-
 
 func _process(_delta):
 	pass
